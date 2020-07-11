ee95aec23cb53cb7db28abc3dbd5185d37fdb7b4
amin havasi <pau.ahq@gmail.com>
2020-07-11 15:06:03 +0430

    login/register

---
+++ src/models/user.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+            minlength: 8,
+        },
+        isAdmin: {
+            type: Boolean,
+            default: false,
+        },
+        tokens: [
+            {
+                _id: false,
+                access: {
+                    type: String,
+                    required: true,
+                },
+                token: {
+                    type: String,
+                    required: true,
+                },
+            },
+        ],
+        date: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+userSchema.statics.findByCredentials = function (email, password) {
+    let User = this;
+    return User.findOne({ email }).then((user) => {
+        if (!user) {
+            return Promise.reject();
+        }
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, user.password, (err, res) => {
+                if (res) {
+                    resolve(user);
+                } else {
+                    reject();
+                }
+            });
+        });
+    });
+};
+userSchema.methods.genAuthToken = function () {
+    let user = this;
+    let access = 'auth';
+    let token = jwt
+        .sign(
+            {
+                _id: user._id.toHexString(),
+                access,
+                isAdmin: user.isAdmin,
+            },
+            process.env.SECRET_KEY
+        )
+        .toString();
+    user.tokens.push({ access, token });
+    return user.save().then(() => {
+        return token;
+    });
+};
+
+userSchema.pre('save', function (next) {
+    let user = this;
+    if (user.isModified('password')) {
+        bcrypt.genSalt(10, (err, salt) => {
+            if (err) throw new Error('something went wrong');
+            bcrypt.hash(user.password, salt, (err, hash) => {
+                if (err) throw new Error('something went wrong');
+
+                user.password = hash;
+                next();
+            });
+        });
+    } else {
+        next();
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,7 +1,65 @@
 const express = require('express');
 const router = express.Router();
-router.get('/', (req, res) => {
-    res.send('amin');
+const persianDate = require('persian-date');
+const _ = require('lodash');
+const User = require('../models/user');
+const Joi = require('@hapi/joi');
+//---------------------------------------------------------
+persianDate.toLocale('en');
+const date = new persianDate().format('YYYY/M/DD');
+const registerValidator = (user) => {
+    const schema = Joi.object({
+        username: Joi.string().max(255).required(),
+        email: Joi.string().email().max(255).required(),
+        password: Joi.string().required().min(8).max(1024),
+    });
+    return schema.validate(user);
+};
+
+const loginValidator = (user) => {
+    const schema = Joi.object({
+        email: Joi.string().email().max(255).required(),
+        password: Joi.string().required().min(8).max(1024),
+    });
+    return schema.validate(user);
+};
+//-------------------------------------------------------------
+router.post('/register', async (req, res) => {
+    try {
+        const { error } = await registerValidator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        const body = await _.pick(req.body, ['username', 'email', 'password']);
+        body.date = date;
+        await User.countDocuments((err, count) => {
+            if (count === 0) {
+                body.isAdmin = true;
+            }
+        });
+        const newUser = await new User(body);
+        await newUser.save();
+        res.status(201).send(newUser);
+    } catch (err) {
+        res.status(400).send(err);
+    }
+});
+
+//login routes-------------------------------------------------------------
+
+router.post('/login', async (req, res) => {
+    try {
+        const { error } = await loginValidator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        const user = await User.findByCredentials(
+            req.body.email,
+            req.body.password
+        );
+
+        const token = await user.genAuthToken();
+
+        res.status(200).send(token);
+    } catch (err) {
+        res.status(400).send(err);
+    }
 });
 
 module.exports = router;
