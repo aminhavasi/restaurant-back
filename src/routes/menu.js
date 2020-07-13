60666ff2f3482b747567231731552d77d8ec9399
amin havasi <pau.ahq@gmail.com>
2020-07-13 17:10:49 +0430

    update menu

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
+const Joi = require('@hapi/joi');
+
 const Menu = require('./../models/menu');
 const auth = require('../middleware/auth');
+//joi-------------------------------------------------
+
+const menuValidator = (menu) => {
+    const schema = Joi.object({
+        name: Joi.string().max(255).required(),
+        cat: Joi.string().max(255).required(),
+        price: Joi.number().required(),
+    });
+    return schema.validate(menu);
+};
+
+//----------------------------------------------------
 
 router.get('/', async (req, res) => {
     const resMenu = [];
@@ -19,6 +33,8 @@ router.get('/', async (req, res) => {
 
 router.post('/create', auth, async (req, res) => {
     try {
+        const { error } = await menuValidator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
         const body = _.pick(req.body, ['name', 'cat', 'price']);
         const newItem = await new Menu(body);
         await newItem.save();
@@ -27,4 +43,27 @@ router.post('/create', auth, async (req, res) => {
         res.send(err);
     }
 });
+
+router.put('/:id', auth, async (req, res) => {
+    try {
+        const { error } = await menuValidator(req.body);
+        if (error) return res.status(400).send(error.details[0].message);
+        const menu = await Menu.findByIdAndUpdate(
+            req.params.id,
+            {
+                cat: req.body.cat,
+                name: req.body.name,
+                price: req.body.price,
+            },
+            { new: true }
+        );
+
+        if (!menu)
+            return res.status(404).send('There is not menu for the given id');
+        res.status(200).send(menu);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
 module.exports = router;
