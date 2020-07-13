f7c981c5529ee809b96309ce438ce0343a22631d
amin havasi <pau.ahq@gmail.com>
2020-07-13 15:20:53 +0430

    menu added

--- src/routes/menu.js
+++ src/routes/menu.js
@@ -2,19 +2,29 @@ const express = require('express');
 const router = express.Router();
 const _ = require('lodash');
 const Menu = require('./../models/menu');
+const auth = require('../middleware/auth');
 
-// router.get('/',async(req,res)=>{
+router.get('/', async (req, res) => {
+    const resMenu = [];
+    try {
+        const menu = await Menu.find({});
+        menu.forEach((menuKey) => {
+            resMenu.push(_.pick(menuKey, ['_id', 'name', 'price', 'cat']));
+        });
+        res.status(200).send(resMenu);
+    } catch (err) {
+        res.send(err);
+    }
+});
 
-// })
-
-router.post('/create', async (req, res) => {
+router.post('/create', auth, async (req, res) => {
     try {
         const body = _.pick(req.body, ['name', 'cat', 'price']);
-        const newItem = new Menu(body);
-        console.log(newItem);
-        newItem.save();
-
-        res.send('ok');
-    } catch (err) {}
+        const newItem = await new Menu(body);
+        await newItem.save();
+        res.status(200).send('success');
+    } catch (err) {
+        res.send(err);
+    }
 });
 module.exports = router;
